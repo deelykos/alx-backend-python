58fc110772f0a8191bbfe68ad2bb2a530c1e01c4
deelykos <fabianohaz@gmail.com>
2024-01-08 23:22:26 +0100

    Basic annotation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,14 @@ Basic annotations - add
 
 
 def add(a: float, b: float) -> float:
+    '''
+    Adds two numbers
+
+    Args:
+        a (float): the first number
+        b (float): the second number
+
+    Returns:
+           float: the sum of the tow numbers
+    '''
     return a + b
