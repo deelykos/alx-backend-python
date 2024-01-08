536d9f63b9ba9b9a739f7904add90b2142e73e27
deelykos <fabianohaz@gmail.com>
2024-01-08 23:31:49 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Basic annotations - concat
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Conncatenates two strings
+
+    Args:
+        str1 (str): first string.
+        str2 (str): second string.
+
+    Returns:
+        str: The concatenated string.
+    '''
+    return str1 + str2
