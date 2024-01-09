4414c1ef4ec7944d1062a1ce7f4e601238b36fcd
deelykos <fabianohaz@gmail.com>
2024-01-09 14:20:36 +0100

    Let's duck type an iterable object

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,4 +7,15 @@ from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''
+    Computes the length of each element in the input list
+    of sequences.
+
+    Args:
+        lst (Iterable[Sequence]): The input iterable containing sequences.
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples where
+        each tuple contains a sequence from the input and its corresponding length.
+    '''
     return [(i, len(i)) for i in lst]
