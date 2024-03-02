1bd8462356d9fb8749e6d0661c2071da56be8ce9
deelykos <fabianohaz@gmail.com>
2024-03-02 15:03:35 +0100

    a coroutine called async_generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""0. Async Generator"""
+
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
+    """Yield a random number between 0 and 10"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
